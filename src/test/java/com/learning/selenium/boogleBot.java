d09c0198312a20c892e6a54e53677a2e782fc726
JayadevTesting4 <JayadevTesting4>
2023-10-30 07:42:14 +0530

    2triel

--- src/test/java/com/learning/selenium/boogleBot.java
+++ src/test/java/com/learning/selenium/boogleBot.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GoogleBot {
+public class boogleBot {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\eclipse-workspace\\SeleniumMavenProject\\binaries\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
